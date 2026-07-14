6d500b6e2cc7fe833912df6de2c260e915cbb4e4
CharmCapsule <mondjoe007@yahoo.com>
2026-07-15 02:17:47 +0400

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,25 +1,11 @@
-import { i18n, type Locale } from "@/i18n-config";
+type Lang = "en" | "de" | "cs";
 
-export const metadata = {
-  title: "i18n within app router - Vercel Examples",
-  description: "How to do i18n in Next.js 15 within app router",
-};
-
-export async function generateStaticParams() {
-  return i18n.locales.map((locale) => ({ lang: locale }));
-}
-
-export default async function Root(props: {
+export default async function Layout({
+  children,
+  params,
+}: {
   children: React.ReactNode;
-  params: Promise<{ lang: Locale }>;
+  params: { lang: Lang };
 }) {
-  const params = await props.params;
-
-  const { children } = props;
-
-  return (
-    <html lang={params.lang}>
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
