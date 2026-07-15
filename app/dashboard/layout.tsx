e564fc8dfa72b587a033008b4e67dedc8a6634e9
CharmCapsule <mondjoe007@yahoo.com>
2026-07-15 09:42:10 +0400

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,20 @@
+import Sidebar from './components/Sidebar'
+import Topbar from './components/Topbar'
+import DashboardShell from './components/DashboardShell'
+import { SidebarProvider } from './components/SidebarContext'
+
 export default function DashboardLayout({ children }) {
   return (
-    <DashboardShell>
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <Topbar />
-        <main className="p-6">
-          {children}
-        </main>
-      </div>
-    </DashboardShell>
+    <SidebarProvider>
+      <DashboardShell>
+        <Sidebar />
+        <div className="flex flex-col flex-1">
+          <Topbar />
+          <main className="p-6">
+            {children}
+          </main>
+        </div>
+      </DashboardShell>
+    </SidebarProvider>
   )
 }
