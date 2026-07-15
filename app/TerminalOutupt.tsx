6443eb950a38d3a1cc6b8dbdace609df69de8dc3
CharmCapsule <mondjoe007@yahoo.com>
2026-07-15 10:09:10 +0400

    Update TerminalOutupt.tsx

--- app/TerminalOutupt.tsx
+++ app/TerminalOutupt.tsx
@@ -1,7 +1,8 @@
 export default function TerminalOutput({ cmd, output }) {
   return (
-    <div className="mb-4">
-      <div className="text-green-500">operator@charm:~$ {cmd}</div>
+    <div className="text-green-500 operator-title">
+  operator@charm:~$ {cmd}
+</div>
       <pre className="text-green-300 whitespace-pre-wrap mt-1">
         {output}
       </pre>
